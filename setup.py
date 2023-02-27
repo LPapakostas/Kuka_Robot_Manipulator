ea172e449180ec5141cd2577bcdd2e6d08279de4
lampis <lampis.papakostas@twi.gr>
2023-02-28 01:51:41 +0200

    Change version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='kuka_manipulator',
     packages=find_packages(include=['kuka_manipulator']),
-    version='0.0.0',
-    description='A example Python package',
+    version='0.0.1',
+    description='Kuka Manipulator Simulation',
     author='Lampis Papakostas',
     author_email='lpapakostas95@yahoo.com',
     license='MIT'
