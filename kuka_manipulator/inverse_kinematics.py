882ba1b9e055a92c191daef814413a6384ea3366
lampis <lampis.papakostas@twi.gr>
2023-02-27 14:16:37 +0200

    Fix Inverse Kinematics

--- kuka_manipulator/inverse_kinematics.py
+++ kuka_manipulator/inverse_kinematics.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from math import sqrt, pow, atan, acos
-from math import sin, cos
-from numpy import arctan2
+from math import sqrt, pow, pi
+from math import sin, cos, atan2
+from typing import List
 
 # *==== CONSTANTS ====*
 
@@ -16,27 +16,44 @@ L6 = 0.100  # in [m]
 L7 = 0.100  # in [m]
 
 
-def compute_inverse_kinematics(x, y, z):
+def compute_inverse_kinematics(x: float, y: float, z: float) -> List[float]:
     """
+    Calculate inverse kinematics for specific arm manipulator
+
+    Parameters
+    ----------
+    x : `double`
+        End Effector x-axis coordinate 
+    y : `double` 
+        End Effector y-axis coordinate 
+    z : `double`
+        End Effector y-axis coordinate 
+
+    Returns
+    -------
+    [q1, q2, q3] : `List`
+        Computed angles for joints 1,2,3
     """
+
     # Define constants for `q1` joint angle
     L = L3 + L6
     K = sqrt(pow(x, 2) + pow(y, 2) - pow(L, 2))
     # Compute q1
-    q1 = arctan2((y*K - x*L), (x*K+y*L))
+    q1 = atan2((K * y - L * x), K * x + L * y)
 
     # Define constants for `q2` joint angle
-    Q = z + K - L0 - L1
-    R = pow((K - L1), 2) + pow((z-L0), 2) - pow(L4, 2) - pow(L5+L7, 2)
-    # Compute q2
-    q2 = acos((Q - sqrt(Q-2*R))/(2*L2))
+    R = (pow((K - L1), 2) + pow((z - L0), 2) -
+         pow(L2, 2) - pow(L4, 2) - pow(L5 + L7, 2)) / (2 * L2)
+    Q = pow(L5 + L7, 2) + pow(L4, 2) - pow(R, 2)
+    # Compute q3
+    q3 = 2 * atan2(-(L5 + L7) + sqrt(Q), R + L4) + 2 * pi
 
     # Define constants for `q3` joint angle
-    A = L0 + L4*sin(q2) + (L5 + L7 + L2)*cos(q2) - z
-    B = L4*cos(q2) - (L5 + L7)*sin(q2)
-    C = L0 + (L2-L4)*sin(q2) - (L5+L7)*cos(q2) - z
-    # Compute q3
-    q3 = 2*atan((-B + sqrt(pow(B, 2) - A*C)) / A)
+    A = L2 + L4 * cos(q3) - (L5 + L7) * sin(q3)
+    B = L4 * sin(q3) + (L5 + L7) * cos(q3)
+    # Compute q2
+    q2 = atan2(A * (K - L1) - B * (z - L0),
+               A * (z - L0) + B * (K - L1))
 
     q_lst = [q1, q2, q3]
     return q_lst
