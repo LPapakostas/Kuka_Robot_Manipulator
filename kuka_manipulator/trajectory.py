3a3c2014f48757d2e47f1e9df54e6b6b10b3d456
lampis <lampis.papakostas@twi.gr>
2023-02-28 13:33:58 +0200

    Create 3step trajectory

--- kuka_manipulator/trajectory.py
+++ kuka_manipulator/trajectory.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import sympy
-from pprint import pprint
+
+# TODO: Add docstrings
 
 
 def generate_trajectory_coeffs(positions, velocities, accelerations, start_time, stop_time):
@@ -19,15 +19,16 @@ def generate_trajectory_coeffs(positions, velocities, accelerations, start_time,
     v_0, v_f = velocities[0], velocities[1]
     a_0, a_f = accelerations[0], accelerations[1]
 
-    coef_matrix = np.array([[1, t_0, t_0 ^ (2), t_0 ^ (3), t_0 ^ (4), t_0 ^ (5)],
-                           [0, 1, 2 * t_0, 3 * t_0 ^
-                               (2), 4 * t_0 ^ (3), 5 * t_0 ^ (4)],
-                           [0, 0, 2, 6*t_0, 12 * t_0 ^ (2), 20 * t_0 ^ (3)],
-                           [1, t_f, t_f ^ (2), t_f ^ (
-                               3), t_f ^ (4), t_f ^ (5)],
-                           [0, 1, 2 * t_f, 3 * t_f ^
-                               (2), 4 * t_f ^ (3), 5 * t_f ^ (4)],
-                           [0, 0, 2, 6*t_f, 12 * t_f ^ (2), 20 * t_f ^ (3)]])
+    coef_matrix = np.array([[1, t_0, t_0**(2), t_0**(3), t_0**(4), t_0**(5)],
+                           [0, 1, 2 * t_0, 3 * t_0 **
+                               (2), 4 * t_0 ** (3), 5 * t_0 ** (4)],
+                           [0, 0, 2, 6 * t_0, 12 * t_0 **
+                               (2), 20 * t_0 ** (3)],
+                           [1, t_f, t_f ** (2), t_f ** (
+                               3), t_f ** (4), t_f ** (5)],
+                           [0, 1, 2 * t_f, 3 * t_f **
+                               (2), 4 * t_f ** (3), 5 * t_f ** (4)],
+                           [0, 0, 2, 6 * t_f, 12 * t_f ** (2), 20 * t_f ** (3)]])
     boundary_conditions = np.array([p_0, v_0, a_0, p_f, v_f, a_f]).T
 
     parameters = np.linalg.inv(coef_matrix)  @ boundary_conditions
@@ -35,64 +36,72 @@ def generate_trajectory_coeffs(positions, velocities, accelerations, start_time,
     return parameters
 
 
-def position_trajectory_expr(coeffs):
+def create_three_phase_trajectory(positions, velocities, accelerations, time, delta=0.2):
     """
     """
-    assert (len(coeffs) == 6)
+    # Decompose values
+    t0, tf = time[0], time[-1]
+    g0, gf = positions[0], positions[-1]
+    g0_dot, gf_dot = velocities[0], velocities[-1]
+    g0_ddot, gf_ddot = accelerations[0], accelerations[-1]
 
-    t = sympy.symbols("t")
-    coeffs_round = [round(x, 2) for x in coeffs]
-    a0, a1, a2 = coeffs_round[0], coeffs_round[1], coeffs_round[2]
-    a3, a4, a5 = coeffs_round[3], coeffs_round[4], coeffs_round[5]
-    g = a0 + a1 * t + a2 * sympy.Pow(t, 2) + \
-        a3*sympy.Pow(t, 3) + a4*sympy.Pow(t, 4) + a5*sympy.Pow(t, 5)
+    #
+    g02_dot = (gf - g0) / (tf - t0 - 2 * delta)
+    g02 = g0 + delta * g02_dot
+    gf2 = g0 + (tf - t0 - 3 * delta) * g02_dot
+    position_trajectory, velocity_trajectory, acceleration_trajectory = [], [], []
 
-    return g
+    # Generate coefficients
+    coeffs_phase1 = generate_trajectory_coeffs(
+        [g0, g02], [g0_dot, g02_dot], [g0_ddot, 0], t0, t0 + 2 * delta)
+    coeffs_phase2 = generate_trajectory_coeffs(
+        [g02, gf2], [g02_dot, g02_dot], [0, 0], t0 + 2 * delta, tf - t0 - 2 * delta)
+    coeffs_phase3 = generate_trajectory_coeffs(
+        [gf2, gf], [g02_dot, gf_dot], [0, gf_ddot], tf - t0 - 2 * delta, tf)
+    assert(len(coeffs_phase1) == len(coeffs_phase2) == len(coeffs_phase3) == 6)
 
+    # Compute position trajectory
+    for ts in time:
+        if(ts < t0 + 2 * delta):
+            current_val = coeffs_phase1[0] + coeffs_phase1[1] * ts + coeffs_phase1[2] * pow(
+                ts, 2) + coeffs_phase1[3] * pow(ts, 3) + coeffs_phase1[4] * pow(ts, 4) + coeffs_phase1[5] * pow(ts, 5)
+        elif(ts < tf - t0 - 2 * delta):
+            current_val = coeffs_phase2[0] + coeffs_phase2[1] * ts
+        else:
+            current_val = coeffs_phase3[0] + coeffs_phase3[1] * ts + coeffs_phase3[2] * pow(
+                ts, 2) + coeffs_phase3[3] * pow(ts, 3) + coeffs_phase3[4] * pow(ts, 4) + coeffs_phase3[5] * pow(ts, 5)
+        position_trajectory.append(current_val)
 
-def velocity_trajectory_expr(coeffs):
-    """
-    """
-    assert (len(coeffs) == 6)
+    # Compute velocity trajectory
+    for ts in time:
+        if(ts < t0 + 2 * delta):
+            current_val = coeffs_phase1[1] + 2 * coeffs_phase1[2] * ts + 6 * coeffs_phase1[3] * pow(
+                ts, 2) + 12 * coeffs_phase1[4] * pow(ts, 3) + 20 * coeffs_phase1[5] * pow(ts, 4)
+        elif(ts < tf - t0 - 2 * delta):
+            current_val = coeffs_phase2[1]
+        else:
+            current_val = coeffs_phase1[1] + 2 * coeffs_phase1[2] * ts + 6 * coeffs_phase1[3] * pow(
+                ts, 2) + 12 * coeffs_phase1[4] * pow(ts, 3) + 20 * coeffs_phase1[5] * pow(ts, 4)
+        velocity_trajectory.append(current_val)
 
-    t = sympy.symbols("t")
-    coeffs_round = [round(x, 2) for x in coeffs]
-    a0, a1, a2 = coeffs_round[0], coeffs_round[1], coeffs_round[2]
-    a3, a4, a5 = coeffs_round[3], coeffs_round[4], coeffs_round[5]
-    g = a1 + 2*a2 * t + \
-        3*a3*sympy.Pow(t, 2) + 4*a4*sympy.Pow(t, 3) + 5*a5*sympy.Pow(t, 4)
+    # Compute acceleration trajectory
+    for ts in time:
+        if(ts < t0 + 2 * delta):
+            current_val = 2 * coeffs_phase1[2] + 6 * coeffs_phase1[3] * ts + 12 * \
+                coeffs_phase1[4] * pow(ts, 2) + 20 * \
+                coeffs_phase1[5] * pow(ts, 3)
+        elif(ts < tf - t0 - 2 * delta):
+            current_val = 0.0
+        else:
+            current_val = 2 * coeffs_phase1[2] + 6 * coeffs_phase1[3] * ts + 12 * \
+                coeffs_phase1[4] * pow(ts, 2) + 20 * \
+                coeffs_phase1[5] * pow(ts, 3)
+        acceleration_trajectory.append(current_val)
 
-    return g
+    trajectories = {
+        "position": position_trajectory,
+        "velocity": velocity_trajectory,
+        "acceleration": acceleration_trajectory
+    }
 
-
-if (__name__ == "__main__"):
-
-    P_A = [1.0, 1.0, 1.0]
-    V_A = [1.0, 0.0, 0.2]
-    A_A = [0.1, 0.05, 0.03]
-
-    P_B = [1.5, 1.5, 1.0]
-    V_B = [2.0, 0.1, 0.3]
-    A_B = [0.0, 0.0, 0.0]
-
-    t_0, t_f = 0, 20  # in [sec]
-
-    x_vals, x_dot_vals, x_ddot_vals = [P_A[0], P_B[0]], [
-        V_A[0], V_B[0]], [A_A[0], A_B[0]]
-    y_vals, y_dot_vals, y_ddot_vals = [P_A[1], P_B[1]], [
-        V_A[1], V_B[1]], [A_A[1], A_B[1]]
-    z_vals, z_dot_vals, z_ddot_vals = [P_A[2], P_B[2]], [
-        V_A[2], V_B[2]], [A_A[2], A_B[2]]
-
-    x_parameters = generate_trajectory_coeffs(
-        x_vals, x_dot_vals, x_ddot_vals, t_0, t_f)
-    y_parameters = generate_trajectory_coeffs(
-        y_vals, y_dot_vals, y_ddot_vals, t_0, t_f)
-    z_parameters = generate_trajectory_coeffs(
-        z_vals, z_dot_vals, z_ddot_vals, t_0, t_f)
-
-    print(f"x(t) = {position_trajectory_expr(x_parameters)}")
-    print("")
-    print(f"y(t) = {position_trajectory_expr(y_parameters)}")
-    print("")
-    print(f"z(t) = {position_trajectory_expr(z_parameters)}")
+    return trajectories
